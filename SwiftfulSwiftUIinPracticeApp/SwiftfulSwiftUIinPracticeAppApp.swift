2bb6ac48edb9e14db56a2226d0ec6dc649f557fb
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-10-12 12:03:30 +0600

    Initial Commit

---
+++ SwiftfulSwiftUIinPracticeApp/SwiftfulSwiftUIinPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIinPracticeAppApp.swift
+//  SwiftfulSwiftUIinPracticeApp
+//
+//  Created by jyotirmoy_halder on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIinPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
