06a25ed33a59c3523f230bf4aac3fdb43df82645
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-10-18 23:13:08 +0600

    bumble chat preview cell and profile image cell added

---
+++ SwiftfulSwiftUIinPracticeApp/Bumble/Components/BumbleChatPreviewCell.swift
@@ -0,0 +1,60 @@
+//
+//  BumbleChatPreviewCell.swift
+//  SwiftfulSwiftUIinPracticeApp
+//
+//  Created by jyotirmoy_halder on 18/10/25.
+//
+
+import SwiftUI
+
+struct BumbleChatPreviewCell: View {
+    
+    var imageName: String = Constants.randomImage
+    var percentageRemaining: Double = Double.random(in: 0...1)
+    var hasNewMessage: Bool = true
+    var userName: String = "Jyoti"
+    var lastChatMessage: String? = "This is the last messag jakdfljaldksjfalfjalsfjlsajflasjdflasjfdlsajlkfj"
+    var isYourMove: Bool = true
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            BumbleProfileImageCell(
+                imageName: imageName,
+                percentageRemaining: percentageRemaining,
+                hasNewMessage: hasNewMessage
+            )
+            
+            VStack(alignment: .leading, spacing: 2) {
+                HStack {
+                    Text(userName)
+                        .font(.headline)
+                        .foregroundStyle(.bumbleBlack)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    if isYourMove {
+                        Text("YOUR MOVE")
+                            .font(.caption2)
+                            .bold()
+                            .padding(.vertical, 4)
+                            .padding(.horizontal, 6)
+                            .background(.bumbleYellow)
+                            .clipShape(RoundedRectangle(cornerRadius: 31))
+                    }
+                }
+                
+                if let lastChatMessage {
+                    Text(lastChatMessage)
+                        .font(.subheadline)
+                        .foregroundStyle(.bumbleGray)
+                        .padding(.trailing, 16)
+                }
+            }
+            .lineLimit(1)
+        }
+    }
+}
+
+#Preview {
+    BumbleChatPreviewCell()
+        .padding()
+}
